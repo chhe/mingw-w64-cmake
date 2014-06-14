d9d7f2c1f625f12e2ac39b73f3006f7293e7d6b9
che <che27012011@googlemail.com>
2014-06-14 13:41:14 +0200

    directory for 64-bit build was wrong

--- start-build-64.sh
+++ start-build-64.sh
@@ -2,7 +2,7 @@
 echo ... starting 64 bit build
 cd ${0%/*}
 mkdir build-64
-cd build-32
+cd build-64
 cmake -DTARGET_ARCH=x86_w64-w64-mingw32 -DCMAKE_INSTALL_PREFIX=../prefix-64 -DMAKEJOBS=4 -G Ninja ..
 ninja mpv
 cd ..
