d2d37ae38ddfc802ef27d3df346a448ca700d636
che <che27012011@googlemail.com>
2014-07-05 19:00:13 +0200

    switching back to two parallel jobs

--- start-build-32.sh
+++ start-build-32.sh
@@ -3,7 +3,7 @@ echo ... starting 32 bit build
 cd ${0%/*}
 mkdir build-32
 cd build-32
-cmake -DTARGET_ARCH=i686-w64-mingw32 -DCMAKE_INSTALL_PREFIX=../prefix-32 -DMAKEJOBS=4 -G Ninja ..
+cmake -DTARGET_ARCH=i686-w64-mingw32 -DCMAKE_INSTALL_PREFIX=../prefix-32 -DMAKEJOBS=2 -G Ninja ..
 ninja mpv
 cd ..
 echo ... 32-bit build finished
--- start-build-64.sh
+++ start-build-64.sh
@@ -3,7 +3,7 @@ echo ... starting 64 bit build
 cd ${0%/*}
 mkdir build-64
 cd build-64
-cmake -DTARGET_ARCH=x86_64-w64-mingw32 -DCMAKE_INSTALL_PREFIX=../prefix-64 -DMAKEJOBS=4 -G Ninja ..
+cmake -DTARGET_ARCH=x86_64-w64-mingw32 -DCMAKE_INSTALL_PREFIX=../prefix-64 -DMAKEJOBS=2 -G Ninja ..
 ninja mpv
 cd ..
 echo ... 64-bit build finished
