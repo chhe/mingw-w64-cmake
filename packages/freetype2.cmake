24221a5f575229cb99e4ef01370b119730fd5252
che <che27012011@googlemail.com>
2017-03-18 22:32:04 +0100

    add SDL2 and SDL2_ttf

--- packages/freetype2.cmake
+++ packages/freetype2.cmake
@@ -1,8 +1,9 @@
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/ft2exec.in ${CMAKE_CURRENT_BINARY_DIR}/ft2exec)
 
 ExternalProject_Add(freetype2
-    DEPENDS libpng zlib
+    DEPENDS zlib
     GIT_REPOSITORY "git://git.sv.gnu.org/freetype/freetype2.git"
+    GIT_TAG 3e79254ae73ba4e5f951967b42d38b0aa1460af8
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${CMAKE_CURRENT_BINARY_DIR}/ft2exec <SOURCE_DIR>/configure
         --build=${HOST_ARCH}
@@ -10,13 +11,13 @@ ExternalProject_Add(freetype2
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
         --without-harfbuzz
+        --without-png
         --with-sysroot=${MINGW_INSTALL_PREFIX}
-        --with-harfbuzz=no
-    BUILD_COMMAND ${MAKE} clean && ${MAKE}
+    BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
         COMMAND ${CMAKE_COMMAND} -E create_symlink ${MINGW_INSTALL_PREFIX}/bin/freetype-config ${CMAKE_INSTALL_PREFIX}/bin/freetype-config
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
-force_rebuild_git(freetype2)
+#force_rebuild_git(freetype2)
 autogen(freetype2)
